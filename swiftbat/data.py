44e951f4fc6d8997d68e9ad26adb7f6f1a700fd8
David Palmer <palmer@lanl.gov>
2022-05-24 09:51:34 -0600

    Working on getting data from heasarc

--- swiftbat/data.py
+++ swiftbat/data.py
@@ -3,9 +3,82 @@ Find and manage Swift data files from Heasarc
 """
 
 from astroquery.heasarc import Heasarc
+import astropy as ap
 import datetime
 from swiftbat.swutil import any2datetime, datetime2mjd
+# import requests
+# import paramiko
+from functools import lru_cache
+from pathlib import Path
+# import urllib
+from swiftbat.generaldir import HTTPDir
 
+
+# swiftdataurl = 'sftp://heasarc.gsfc.nasa.gov/FTP/swift/data'
+# @lru_cache
+# def _sshclient_heasarc(url):
+#     ssh = paramiko.SSHClient()
+#     try:
+#         ssh.load_host_keys(str(Path('~/.ssh/known_hosts').expanduser()))
+#     except:
+#         pass    # optional
+#     parsedurl = urllib.parse.urlparse(str(url).strip('/'))
+#     ssh.connect(parsedurl.hostname,
+#                 password=parsedurl.password,
+#                 username=parsedurl.username)
+#     return ssh, parsedurl.path
+
+# @lru_cache
+# def _sftp_heasarc_swiftdata(url=swiftdataurl, subdir=None):
+#     ssh, path = _sshclient_heasarc(url)   # Cached
+#     sftp = ssh.open_sftp(url)
+#     subdir = "/".join(([path] if path else [])
+#                    + ([subdir] if subdir else [])).strip("/")
+#     if subdir:
+#         sftp.chdir(subdir)
+#     sftp.chdir = None   # Prevent chdir from being further applied to this cached object
+#     return sftp
+
+@lru_cache
+def heasarc_tdrss_dir():
+    return HTTPDir("https://heasarc.gsfc.nasa.gov/FTP/swift/data/tdrss")
+
+def _heasarc_TTE_location(trignum:int, time:datetime.datetime, matchstring="bevshsp_uf.evt"):
+    """The URL for time-tagged-event data for a specific trigger
+    trignum: Trigger number (or TARGET_ID)
+    time: datetime of trigger
+    matchstring: must be a straight string or a python regex (i.e. not a shell wildcard string) found in filename
+    """
+    time = time.replace(tzinfo=None)    # Make it naive
+    monthstring = f"{time:%Y_%m}"
+    tdrssdir = heasarc_tdrss_dir()
+    try:
+        subdir = f"{monthstring}/{trignum:08d}000"
+        matches = tdrssdir.matchPath(subpath=subdir, wildpath=f'.*{matchstring}.*')
+        # tdrsslist = sftp.listdir(subdir)
+        # for fname in tdrsslist:
+        #     if matchstring in fname:
+        #         return (swiftdataurl, subdir, fname)
+    except Exception as e:
+        pass
+    age = datetime.datetime.utcnow() - time
+    
+    
+    
+    
+
+
+
+
+
+# , triggerrecord=None, **queryargs):
+#     trignums = []   # Trigger number list
+#     times = []      # approximate datetime list
+#     if triggerrecord is None:
+        
+#     if isinstance(triggerrecord, ap.table.Row):
+#         triggerrecord = [triggerrecord] # Make it iterable
+    
 
 def heasarcquery(cache=True, **queryargs):
     heasarc = Heasarc()
@@ -67,8 +140,16 @@ def triggerrecords(time=None, timewindow_d=1/24, trignum=None, cache=True, **que
     return heasarcquery(cache=cache, **queryargs)
 
 def main_test():
+    import swiftbat
     result_trigrange = triggerrecords(trignum=[1104735, 1104873])
     result_timerange = triggerrecords(time=["2022-05-01", "2022-05-02"])
+    # Look at one row
+    r = result_trigrange[2]
+    location = _heasarc_TTE_location(r['TARGET_ID'], swiftbat.mjd2datetime(r['TIME']))
+    
+    timechecks = [(swiftbat.mjd2datetime(r['TIME']) - swiftbat.met2datetime(r['TIME_SECONDS'], correct=True)).total_seconds()
+                  for r in result_trigrange]
+    
     pass
     
 if __name__ == '__main__':
